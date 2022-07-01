24a1e82e5d6cd03660e011b2c4bee1ac79b37b98
Sachin Bhatt <sach.m.bhatt@gmail.com>
2022-07-01 10:16:50 -0700

    removed unnecessary lines

--- main.py
+++ main.py
@@ -36,9 +36,7 @@ def hello_worlds(stock):
     url = u + x
     r = requests.get(url)
     data = r.json()
-    pd.set_option('display.width', 1000)
-    pd.set_option('colheader_justify', 'center')
-
+   
     for info in data:
         stockinfo.append([data['Global Quote']['01. symbol'], data['Global Quote']['05. price'], data['Global Quote']['02. open'], data['Global Quote']['03. high'], data['Global Quote']['04. low'], data['Global Quote']['06. volume'], data['Global Quote']['07. latest trading day'], data['Global Quote']['08. previous close'], data['Global Quote']['09. change'], data['Global Quote']['10. change percent'], t1])
     df = pd.DataFrame(data=stockinfo, columns=['Symbol', 'Open', 'High', 'Low', 'Price', 'Volume', 'Latest trading day', 'Previous close', 'Change', 'Change percent', 'Entered'])
@@ -51,55 +49,13 @@ def hello_worlds(stock):
     return render_template('index2.html', column_names=df.columns.values, row_data=list(df.values.tolist()), zip=zip, stockinfo=stockinfo, df=df, ip=ip)
 
 
-# @app.route('/view', methods=['POST'])
-# def my_form_post():
-#
-#     if request.method == "POST":
-#
-#         try:
-#             text = request.form['stock']
-#             stock = text.upper()
-#             url = 'http://192.168.0.100:8080/'+ stock
-#             # r = requests.get('http://192.168.0.100:8080/'+stock)
-#         except:
-#             print("An exception occurred")
-#
-#     return redirect(url, code=302)
-
-
-
 @app.route('/view', methods=("POST", "GET"))
 def hello_worldn():
     df = pd.DataFrame(data=stockinfo, columns=['Symbol', 'Open', 'High', 'Low', 'Price', 'Volume', 'Latest trading day',
                                                'Previous close', 'Change', 'Change percent', 'Entered'])
     df = (df.drop_duplicates(subset='Symbol', keep='last'))
-    
-            # df2 = (df2.reset_index()
-            #        .drop_duplicates(subset='Symbol', keep='last')
-            #        .set_index('index'))
-    # pd.set_option('display.width', 1000)
-    # pd.set_option('colheader_justify', 'center')
-
+              
     return render_template('index2.html', column_names=df.columns.values, row_data=list(df.values.tolist()), zip=zip, stockinfo=stockinfo, df=df, ip=ip)
-
-
-# @app.route('/c/<stock>')
-# def hello_world2():
-#     api_key = secapi.api_key
-#     u = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='
-#     x = stock + '&apikey=' + api_key
-#     r = requests.get(url)
-#     json_data = json.loads(r.text)
-#     data = json_data.json()
-#     # for key in data:
-#     #     print(key)
-#     #     for kk in data[i]:
-#     #          print(kk)
-#     print(type(data))
-#     print(type(json_data))
-#     return json_data
-
-
 
 
 @app.route('/run', methods=("POST", "GET"))
@@ -107,7 +63,7 @@ def hello_world5(stockinfo=stockinfo):
    
     pd2 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
     first_table = pd2[0]
-    second_table = pd2[1]
+   
     df = first_table
     stocks = df['Symbol'].values.tolist()
     shuffle(stocks)
@@ -130,22 +86,6 @@ def hello_world5(stockinfo=stockinfo):
                               columns=['Symbol', 'Price', 'Open', 'High', 'Low', 'Volume', 'Latest trading day',
                                        'Previous close', 'Change', 'Change percent', 'Entered'])
             df = (df.drop_duplicates(subset='Symbol', keep='last'))
-
-
-            # df2 = pd.DataFrame(data=stockinfo,
-            #                   columns=['Symbol', 'AssetType', 'Name', 'Description', 'CIK', 'Exchange', 'Currency',
-            #                            'Country', 'Sector', 'Industry',
-            #                            'Address', 'FiscalYearEnd', 'LatestQuarter',
-            #                            'MarketCapitalization', 'EBITDA', 'PERatio',
-            #                            'PEGRatio', 'BookValue', 'DividendPerShare',
-            #                            'DividendYield', 'EPS', 'RevenuePerShareTTM',
-            #                            'ProfitMargin', 'OperatingMarginTTM', 'ReturnOnAssetsTTM',
-            #                            'ReturnOnEquityTTM', 'RevenueTTM', 'GrossProfitTTM',
-            #                            'DilutedEPSTTM', 'QuarterlyEarningsGrowthYOY', 'QuarterlyRevenueGrowthYOY', 'AnalystTargetPrice', 'TrailingPE', 'ForwardPE',
-            #                            'PriceToSalesRatioTTM', 'PriceToBookRatio', 'EVToRevenue', 'EVToEBITDA', 'Beta', '52WeekHigh',
-            #                            '52WeekLow', '50DayMovingAverage', '200DayMovingAverage', 'SharesOutstanding', 'DividendDate', 'ExDividendDate'])
-            #
-            # df3 = df.set_index('Symbol').join(df2.set_index('Symbol'))
 
 
             print(r.status_code)
@@ -175,29 +115,7 @@ def hello_world5(stockinfo=stockinfo):
                 print(time.localtime())
                 print(r.status_code)
                 print('Inner else')
-         # stockindf = df2['Symbol'].values.tolist()
-         # a = np.array(stockindf)
-         # da = pd.DataFrame(np.expand_dims(a, axis=0), columns=['Symbol', 'Open', 'High', 'Low', 'Price', 'Volume', 'Latest trading day', 'Previous close', 'Change', 'Change percent'])
-         # ddf = df2.drop_duplicates()
-         # result = pd.join([ddf, da]).shape[0] - pd.join([ddf, da]).drop_duplicates().shape[0]
-         # print(result)
-
-            # print(df2)
-            # print(stockinfo)
-                # data2 = (
-                #         f"Ticker is..: {stock}  \n"
-                #         f"Ticker day-open : {data['Global Quote']['02. open']} \n"
-                #         f"Ticker day-high : {data['Global Quote']['03. high']} \n"
-                #         f"Ticker day-low : {data['Global Quote']['04. low']} \n"
-                #         f"The price of {stock} is  {data['Global Quote']['05. price']} \n"
-                #         f"Ticker volume is : {data['Global Quote']['06. volume']} \n"
-                #         f"Latest treding day : {data['Global Quote']['07. latest trading day']} \n"
-                #         f"Ticker previous close : {data['Global Quote']['08. previous close']} \n"
-                #         f"Ticker change : {data['Global Quote']['09. change']} \n"
-                #         f"Ticker change percent : {data['Global Quote']['10. change percent']}"
-                #
-                # )
-
+       
         else:
             print('Outer else')
             print(r.status_code)
