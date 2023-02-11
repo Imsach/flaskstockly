3f99f1ab25fe861542c95a7600baeffc1920585a
Imsach <sach.m.bhatt@gmail.com>
2023-02-11 03:14:28 -0800

    added Major holders, charts and change to templates

--- main.py
+++ main.py
@@ -30,12 +30,16 @@ from selenium import webdriver # Used to control a browser programmatically.
 from selenium.webdriver.chrome.options import Options # Used to set options for the Chrome browser when using Selenium.  
 import plotly  # Used for data visualization.
 import plotly.express as px # Used for data visualization with Plotly library.
-import os
+import os 
 import yfinance as yf
 import datetime as dt
 import sqlite3
 from flask import jsonify
 import plotly.graph_objs as go
+import ta
+
+
+
 
 # Initialize Flask application and set global variables
 
@@ -256,10 +260,11 @@ if not os.path.exists("stock_data.db"):
     # Connect to the database
     conn = sqlite3.connect('stock_data.db')
     # Create the tables
-    conn.execute("CREATE TABLE stock_data (Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Adj Close REAL, Volume INTEGER, symbol TEXT, Dividends REAL, Stock Splits REAL, Capital Gains REAL)")
+    conn.execute("CREATE TABLE stock_data (Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Adj Close REAL, Volume INTEGER, symbol TEXT, Dividends REAL, Stock Splits REAL, Capital Gains REAL, Avg_volume REAL, Upper Band REAL, Lower Band REAL)")
     conn.execute("CREATE TABLE news_data (symbol TEXT, title TEXT, publisher TEXT, link TEXT, providerPublishTime TIMESTAMP, relatedTickers TEXT)")
     conn.execute("CREATE TABLE mutualfund_data (Holder TEXT, Shares REAL, Date Reported TIMESTAMP, '% Out' REAL, Value REAL)")
-
+    conn.execute("CREATE TABLE majorholders_data (numbers TEXT, title TEXT, symbol TEXT)")
+    conn.execute("CREATE TABLE stocksplits_data (Date TEXT, 'Stock Splits' REAL, symbol TEXT)")
     conn.commit()
     
 else:
@@ -273,16 +278,26 @@ else:
     if cursorNews.fetchone():
         conn.execute("DELETE FROM news_data")
         conn.commit()
-    mutualfundNews = conn.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='mutualfund_data'")
-    if mutualfundNews.fetchone():
+    cursorMutualfund = conn.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='mutualfund_data'")
+    if cursorMutualfund.fetchone():
         conn.execute("DELETE FROM mutualfund_data")
+        conn.commit()
+    cursorMajorHolders = conn.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='majorholders_data'")
+    if cursorMajorHolders.fetchone():
+        conn.execute("DELETE FROM majorholders_data")
+        conn.commit()
+    cursorStockSplits = conn.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='stocksplits_data'")
+    if cursorStockSplits.fetchone():
+        conn.execute("DELETE FROM stocksplits_data")
         conn.commit()
 
 conn = sqlite3.connect('stock_data.db')
 
 image_HTML = 'static/volume_price.html'
+image_HTML2 = 'static/rsi.html'
 if os.path.exists(image_HTML):
     os.remove(image_HTML)
+    os.remove(image_HTML2)
 
 # Route for retrieving data from the database
 @app.route('/stock_data', methods=['GET'])
@@ -293,22 +308,33 @@ def stock_data():
     query = "SELECT * FROM stock_data"
     queryNews = "SELECT * FROM news_data"
     queryMutualfund = "SELECT * FROM mutualfund_data"
-
+    queryMajorHolders = "SELECT * FROM majorholders_data"
+    queryStockSplits = "SELECT * FROM stocksplits_data"
+    
     if symbol:
         query += f" WHERE symbol='{symbol}'"
         stock = yf.Ticker(symbol)
         end_date = dt.datetime.now().strftime('%Y-%m-%d')
-        data = stock.history(start='2023-01-01',end=end_date)
+        data = stock.history(start='2022-11-01',end=end_date)
         data.reset_index(level=0, inplace=True)
         data["Date"] = data["Date"].apply(lambda x: x.strftime('%Y-%m-%d'))
+        data['Avg_volume'] = data['Volume'].rolling(window=14).mean()
+        data["Rsi"] = ta.momentum.RSIIndicator(data["Close"]).rsi()
         data.drop(["Dividends", "Stock Splits", "Capital Gains"], axis=1, inplace=True, errors='ignore')
-        data.drop(["Capital Gains"], axis=1, inplace=True, errors='ignore')
         # Plot the data using Plotly
-        fig1 = go.Scatter(x=data['Date'], y=data['Volume'], mode='lines', name='Volume')
-        fig2 = go.Scatter(x=data['Date'], y=data['Close'], mode='lines', name='Price', yaxis='y2')
-        layout = go.Layout(title='Stock Volume and Price', template='plotly_dark', xaxis=dict(title='Date'), yaxis=dict(title='Volume (in millions)'), yaxis2=dict(title='Price', overlaying='y', side='right'))
-        fig = go.Figure(data=[fig1, fig2], layout=layout)
+        fig1 = go.Scatter(x=data['Date'], y=data['Volume'], mode='lines+markers', name='Volume', line=dict(color='white'))
+        fig2 = go.Scatter(x=data['Date'], y=data['Close'], mode='lines', name='Price', yaxis='y2', line=dict(color='red'))
+        fig3 = go.Scatter(x=data['Date'], y=data['Avg_volume'], mode='lines', opacity=0.4, name='AvgVolume', line=dict(color='yellow', dash='dash'))
+        fig4 = go.Scatter(x=data['Date'], y=data['Rsi'], mode='lines+markers', name='RSI', line=dict(color='cyan'))
+        fig5 = go.Scatter(x=data['Date'], y=[20]*len(data['Date']), mode='lines', name='RSI20', line=dict(color='white', dash='dash'))
+        fig6 = go.Scatter(x=data['Date'], y=[80]*len(data['Date']), mode='lines', name='RSI80', line=dict(color='red', dash='dash'))
+
+        layout = go.Layout(title='Stock Volume and Price - ' + symbol, template='plotly_dark', xaxis=dict(title='Date'), yaxis=dict(title='Volume (in millions)'), yaxis2=dict(title='Price', overlaying='y', side='right'))
+        layout2 = go.Layout(template='plotly_dark', xaxis=dict(title='Date'), yaxis=dict(title='RSI', overlaying='y', side='right'))
+        fig = go.Figure(data=[fig1, fig2, fig3], layout=layout)
         fig.write_html(image_HTML)
+        fig2 = go.Figure(data=[fig4, fig5, fig6], layout=layout2)
+        fig2.write_html(image_HTML2)
         # Adding data to the database
         data['symbol'] = stock.ticker
         print(data)
@@ -318,36 +344,56 @@ def stock_data():
         news_df.drop(["uuid", "type", "thumbnail"], axis=1, inplace=True, errors='ignore')
         news_df['providerPublishTime'] = pd.to_datetime(news_df['providerPublishTime'], unit='s')
         news_df["providerPublishTime"] = news_df["providerPublishTime"].apply(lambda x: x.strftime('%Y-%m-%d'))
-        news_df['relatedTickers'] = news_df['relatedTickers'].apply(lambda x: ",".join(x))
+        news_df['relatedTickers'] = news_df['relatedTickers'].apply(lambda x: ",".join(x) if isinstance(x, (list, tuple)) else x)
         news_df['symbol'] = stock.ticker
         news_df.to_sql('news_data', conn, if_exists='replace', index=False)
         queryNews += f" WHERE symbol='{symbol}'"
         queryMutualfund += f" WHERE symbol='{symbol}'"
         mutualfund = stock.get_mutualfund_holders()
         mutualfund_df = pd.DataFrame(mutualfund)
-        mutualfund_df['symbol'] = stock.ticker
-        mutualfund_df["Date Reported"] = mutualfund_df["Date Reported"].apply(lambda x: x.strftime('%Y-%m-%d'))
-        mutualfund_df["% Out"] = mutualfund_df["% Out"].apply(lambda x: 100 * float(x))
-        mutualfund_df["% Out"] = mutualfund_df["% Out"].apply(lambda x: "{:,.3f} %".format(x))
-        
-        mutualfund_df.to_sql('mutualfund_data', conn, if_exists='replace', index=False)
-
-
-
-
+        if not mutualfund_df.empty:
+            mutualfund_df['symbol'] = stock.ticker
+            mutualfund_df["Date Reported"] = mutualfund_df["Date Reported"].apply(lambda x: x.strftime('%Y-%m-%d'))
+            mutualfund_df["% Out"] = mutualfund_df["% Out"].apply(lambda x: 100 * float(x))
+            mutualfund_df["% Out"] = mutualfund_df["% Out"].apply(lambda x: "{:,.3f} %".format(x))
+            mutualfund_df["Value"] = mutualfund_df["Value"].apply(lambda x: float(x) / 1000000)
+            mutualfund_df["Value"] = mutualfund_df["Value"].apply(lambda x: "{:,.2f} million$".format(x))
+            mutualfund_df.to_sql('mutualfund_data', conn, if_exists='replace', index=False)
+        majorHolders = stock.get_major_holders()
+        majorHolders_df = pd.DataFrame(majorHolders)
+        majorHolders_df['symbol'] = stock.ticker
+        majorHolders_df.to_sql('majorholders_data', conn, if_exists='replace', index=False)
+        queryMajorHolders += f" WHERE symbol='{symbol}'"
+        queryStockSplits += f" WHERE symbol='{symbol}'"     
+        stockSplits = stock.get_splits()
+        print(stockSplits)
+        stockSplits_df = pd.DataFrame(stockSplits)
+        stockSplits_df.reset_index(level=0, inplace=True)
+        stockSplits_df['Date'] = stockSplits_df['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        stockSplits_df['symbol'] = stock.ticker
+        stockSplits_df.to_sql('stocksplits_data', conn, if_exists='replace', index=False)
+            
 
     chart_exists = os.path.exists('static/volume_price.html')
+    chart_exists2 = os.path.exists('static/rsi.html')
     cursor = conn.execute(query)
     cursorNews = conn.execute(queryNews)
     cursorMutualfund = conn.execute(queryMutualfund)
+    cursorMajorHolders = conn.execute(queryMajorHolders)
+    cursorStockSplits = conn.execute(queryStockSplits)
+
+
+
 
     data = cursor.fetchall()
     newsData = cursorNews.fetchall()
     mutualfundData = cursorMutualfund.fetchall()
+    majorHolders = cursorMajorHolders.fetchall()
+    stockSplits = cursorStockSplits.fetchall()
 
     conn.close()
     
-    return render_template("index3.html", data=data, newsData=newsData, mutualfundData = mutualfundData, symbol=symbol, chart_exists=chart_exists)
+    return render_template("index3.html", data=data, newsData=newsData, mutualfundData=mutualfundData, majorHolders=majorHolders, stockSplits=stockSplits, symbol=symbol, chart_exists=chart_exists, chart_exists2=chart_exists2)
 
 
 '''
